01e7e7458442e9175732c77cadc6da8a46707f10
sahira20198763 <sahirareyesmedina@gmail.com>
2020-06-02 20:43:48 -0400

    primer parcial ejercicios

---
+++ ejercicio 1 del parcial/ejercicio 1 del parcial/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1_del_parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Pedir al usuario el precio de un producto y la forma de pagar(efectivo o tarjeta) si la forma de pago es mediante tarjeta, pedir el numero de tarjeta(12 digitos). Ojo, validar que el valor ingresado en el sea positivo.*/
+            double precio, efec, tar, pag;
+
+            Console.WriteLine("Digite el precio del producto a pagar");
+            precio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Diga que forma usted quiere pagar:");
+            Console.WriteLine("Efectivo marque 1");
+            Console.WriteLine("Tarjeta marque 2");
+            pag = Convert.ToDouble(Console.ReadLine());
+
+            if (pag == 1)
+            {
+                Console.WriteLine("Usted ha elegido el  pago en efectivo:");
+                Console.WriteLine("Digite el monto");
+                efec = Convert.ToDouble(Console.ReadLine());
+                if (efec > 0)
+                {
+                    Console.WriteLine("El pago es positivo");
+                }
+                else { Console.WriteLine("El monto es negativo"); }
+            }
+            else if (pag == 2)
+            {
+
+                Console.WriteLine("Usted ha elegido el pago por tarjeta:");
+                Console.WriteLine("Digite el numero de la tarjeta");
+                tar = Convert.ToDouble(Console.ReadLine());
+
+
+                if ((tar >= 100000000000) && (tar <= 199999999999))
+                {
+                    ;
+                    Console.WriteLine("");
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Faltan dígitos o digitó de más");
+                }
+
+            }
+            else { Console.WriteLine("La opción que usted ingresó, no se encuentra disponible."); }
+
+            Console.Read();
+        }
+    }
+}
---
+++ ejercicio 2 del parcial/ejercicio 2 del parcial/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2_del_parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Crear un programa que pida el nombre de un dia de la semana e identifique si es fin de semana o no. En caso de error, indicarlo  */
+
+            string diasm;
+            Console.WriteLine("Escribe un dia de la semana");
+            diasm = Console.ReadLine();
+
+            switch (diasm)
+            {
+
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+
+                default:
+                    Console.WriteLine("No se encuentra el dia solicitado");
+                    break;
+            }
+
+
+            Console.Read();
+        }
+    }
+}
